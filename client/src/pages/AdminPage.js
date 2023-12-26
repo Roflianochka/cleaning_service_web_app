ea9c64af93b869ff044ea1d1e26e87e3f2bbdfef
Roflianochka <maximbelaev831@gmail.com>
2023-12-26 20:49:09 +0300

    feat: add visiable instruments for admin functionality

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,6 +1,40 @@
 import React from "react";
+import { Button, Container } from "react-bootstrap";
+import { useState } from "react";
+import CreateService from "../components/modals/CreateService";
+import CreateCategory from "../components/modals/CreateCategory";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  const [categoryVisible, setCategoryVisible] = useState(false);
+  const [serviceVisible, setServiceVisible] = useState(false);
+
+  return (
+    <Container className="d-flex flex-column">
+      <Button
+        variant={"outline-dark"}
+        className="mt-4 p-2"
+        onClick={() => setCategoryVisible(true)}
+      >
+        Добавить категорию
+      </Button>
+      <Button
+        variant={"outline-dark"}
+        className="mt-4 p-2"
+        onClick={() => setServiceVisible(true)}
+      >
+        Добавить услугу
+      </Button>
+
+      <CreateCategory
+        show={categoryVisible}
+        onHide={() => setCategoryVisible(false)}
+      />
+      <CreateService
+        show={serviceVisible}
+        onHide={() => setServiceVisible(false)}
+      />
+    </Container>
+  );
 };
 
 export default AdminPage;
