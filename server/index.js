771d1f75763077d7f6c0e46dc22f11ca9e40a2fc
Roflianochka <maximbelaev831@gmail.com>
2023-12-26 21:32:33 +0300

    refactor: update main server index.js file logic

--- server/index.js
+++ server/index.js
@@ -5,13 +5,17 @@ const sequelize = require("./db");
 const PORT = process.env.PORT || 5000;
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/errorHandlingMiddleware')
+const path = require('path')
 
 const app = express();
 app.use(cors())
 app.use(express.json())
-app.use('/api',router)
+app.use(fileUpload({}))
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use('/api', router)
 app.use(errorHandler)
 
 
