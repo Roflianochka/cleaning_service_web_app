32a68251210ccc02920c057c55bdc4c3af05b327
Roflianochka <maximbelaev831@gmail.com>
2023-12-30 18:34:14 +0300

    refactor: update NavBar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,36 +1,59 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "..";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
+import { ADMIN_ROUTE, FAQ_ROUTE, LOGIN_ROUTE, MAIN_ROUTE, USER_ROUTE } from "../utils/consts";
 import Button from "react-bootstrap/Button";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
   const navigate = useNavigate();
 
+  const token = localStorage.getItem('token');
+  const decodedToken = token ? jwtDecode(token) : null;
+
   const logout = () => {
-    user.setUser({});
     user.setIsAuth(false);
+    user.setUser({});
+    localStorage.clear();
+    navigate(MAIN_ROUTE)
   };
 
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <NavLink style={{ color: "white" }} to={SHOP_ROUTE}>
+        <NavLink style={{ color: "white" }} to={MAIN_ROUTE}>
           CleaningCompany
         </NavLink>
-        {user.isAuth ? (
+        {decodedToken ? (
           <Nav className="ml-auto" style={{ color: "white" }}>
+            {decodedToken.role === 'ADMIN' ?
+              <Button
+                variant={"outline-light"}
+                onClick={() => navigate(ADMIN_ROUTE)}
+              >
+                Админ
+              </Button> : <></>
+            }
             <Button
               variant={"outline-light"}
-              onClick={() => navigate(ADMIN_ROUTE)}
+              onClick={() => navigate(USER_ROUTE)}
+              className="ms-4"
             >
-              Админ
+              Профиль
+            </Button>
+            <Button
+              className="ms-4"
+              style={{ color: "white" }}
+              onClick={() => navigate(FAQ_ROUTE)}
+              variant='outline-light'
+            >
+              FAQ
             </Button>
             <Button
               variant={"outline-light"}
