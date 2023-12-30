92903ac3f693b7afa515a378b33066fad77b58a1
Roflianochka <maximbelaev831@gmail.com>
2023-12-30 18:32:26 +0300

    refactor:  implement admin page functionality

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -3,10 +3,16 @@ import { Button, Container } from "react-bootstrap";
 import { useState } from "react";
 import CreateService from "../components/modals/CreateService";
 import CreateCategory from "../components/modals/CreateCategory";
+import DeleteService from "../components/modals/DeleteService";
+import DeleteCategory from "../components/modals/DeleteCategory";
+import { EMPLOYEES_ROUTE, APPOINTMENTS_ROUTE } from "../utils/consts";
+import { Link } from "react-router-dom";
 
 const AdminPage = () => {
   const [categoryVisible, setCategoryVisible] = useState(false);
   const [serviceVisible, setServiceVisible] = useState(false);
+  const [deleteCategory, setDeleteCategory] = useState();
+  const [deleteService, setDeleteService] = useState();
 
   return (
     <Container className="d-flex flex-column">
@@ -17,12 +23,30 @@ const AdminPage = () => {
       >
         Добавить категорию
       </Button>
+      <Button onClick={() => setDeleteCategory(true)} variant={"outline-dark"}
+        className="mt-4 p-2">
+        Удалить категорию
+      </Button>
       <Button
         variant={"outline-dark"}
         className="mt-4 p-2"
         onClick={() => setServiceVisible(true)}
       >
         Добавить услугу
+      </Button>
+      <Button onClick={() => setDeleteService(true)} variant={"outline-dark"}
+        className="mt-4 p-2">
+        Удалить услугу
+      </Button>
+
+      <Button as={Link} to={EMPLOYEES_ROUTE} variant={"outline-dark"}
+        className="mt-4 p-2">
+        Сотрудники
+      </Button>
+
+      <Button as={Link} to={APPOINTMENTS_ROUTE} variant={"outline-dark"}
+        className="mt-4 p-2">
+        Заказы
       </Button>
 
       <CreateCategory
@@ -32,6 +56,14 @@ const AdminPage = () => {
       <CreateService
         show={serviceVisible}
         onHide={() => setServiceVisible(false)}
+      />
+      <DeleteCategory
+        show={deleteCategory}
+        onHide={() => setDeleteCategory(false)}
+      />
+      <DeleteService
+        show={deleteService}
+        onHide={() => setDeleteService(false)}
       />
     </Container>
   );
