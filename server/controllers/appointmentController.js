05923bead6e3ab3d06820bdda5b11291dec7200a
Roflianochka <maximbelaev831@gmail.com>
2023-12-30 17:58:44 +0300

    feat: add fetchPendingAppointments endpoint

--- server/controllers/appointmentController.js
+++ server/controllers/appointmentController.js
@@ -1,19 +1,25 @@
-const ApiError = require("../error/ApiError.js");
-const { Appointments } = require("../models/models.js");
+const {
+  Appointments,
+  Services,
+  User,
+  ServiceAssignment,
+} = require("../models/models.js");
 
 class AppointmentController {
   async create(req, res) {
     try {
-      const { user_id, service_id, employee_id, appointment_datetime } = req.body;
+      const { user_id, service_id, employee_id, appointment_datetime, price } =
+        req.body;
 
-      let status = "PENDING"
+      let status = "PAYMENT_NEEDED";
 
       const appointment = await Appointments.create({
         appointment_datetime,
         employee_id,
         service_id,
         user_id,
-        status
+        status,
+        price,
       });
 
       return res.json(appointment);
@@ -25,14 +31,22 @@ class AppointmentController {
       }
     }
   }
+
   async getAll(req, res) {
     try {
-      const appointments = await Appointments.findAll();
+      const appointments = await Appointments.findAll({
+        include: [
+          { model: User, attributes: ["first_name", "last_name", "email"] },
+          { model: Services, attributes: ["service_name"] },
+        ],
+      });
+
       res.json(appointments);
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
   }
+
   async getOne(req, res) {
     const { id } = req.params;
     try {
@@ -45,38 +59,86 @@ class AppointmentController {
       return res.status(500).json({ message: error.message });
     }
   }
+
   async updateById(req, res) {
     const { id } = req.params;
-    const { user_id, service_id, employee_id, appointment_datetime, status } =
-      req.body;
+    const status = "COMPLETED";
     try {
       const appointment = await Appointments.findByPk(id);
       if (!appointment) {
         return res.status(404).json({ message: "Appointment not found" });
       }
+
       await appointment.update({
-        user_id,
-        service_id,
-        employee_id,
-        appointment_datetime,
         status,
       });
+
+      const serviceAssignment = await ServiceAssignment.findOne({
+        where: {
+          service_id: appointment.service_id,
+          assignment_date: appointment.appointment_datetime,
+        }
+      });
+
+      if (serviceAssignment) {
+        await serviceAssignment.destroy();
+      }
+
       res.json(appointment);
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
   }
+
   async deleteById(req, res) {
     const { id } = req.params;
     try {
       const appointment = await Appointments.findByPk(id);
+
       if (!appointment) {
         return res.status(404).json({ message: "Appointment not found" });
       }
+
+      const serviceAssignment = await ServiceAssignment.findOne({
+        where: {
+          service_id: appointment.service_id,
+          assignment_date: appointment.appointment_datetime,
+        },
+      });
+
+      if (serviceAssignment) {
+        await serviceAssignment.destroy();
+      }
+
       await appointment.destroy();
+
       res.sendStatus(204);
     } catch (error) {
       return res.status(500).json({ message: error.message });
+    }
+  }
+
+  async fetchPendingAppointments(req, res) {
+    try {
+      const pendingAppointments = await Appointments.findAll({
+        where: {
+          status: "PENDING",
+        },
+        include: [
+          {
+            model: Services,
+            attributes: ["service_name"],
+          },
+          {
+            model: User,
+            attributes: ["first_name", "last_name", "email"],
+          },
+        ],
+      });
+
+      return res.json(pendingAppointments);
+    } catch (error) {
+      throw error;
     }
   }
 }
--- server/routes/appointmentsRouter.js
+++ server/routes/appointmentsRouter.js
@@ -7,5 +7,6 @@ router.get("/getAll", appointmentController.getAll);
 router.get("/getById/:id", appointmentController.getOne);
 router.put("/updateById/:id", appointmentController.updateById);
 router.delete("/deleteById/:id", appointmentController.deleteById);
+router.get('/fetchPending', appointmentController.fetchPendingAppointments)
 
 module.exports = router;
