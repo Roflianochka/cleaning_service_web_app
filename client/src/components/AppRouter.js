bc3e4a29b6b7d1de002bf4d0c5bfb09bfff330a3
Roflianochka <maximbelaev831@gmail.com>
2023-12-30 18:12:43 +0300

    refactor: optimize AppRouter component

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,21 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
-import { SHOP_ROUTE } from "../utils/consts";
-import { Context } from "../index";
+import { MAIN_ROUTE } from "../utils/consts";
+
 const AppRouter = () => {
-  const { user } = useContext(Context);
-  console.log(user);
   return (
     <Routes>
-      {user.isAuth &&
-        authRoutes.map(({ path, Component }) => (
-          <Route key={path} path={path} element={<Component />} />
-        ))}
-      {publicRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} element={<Component />} />
-      ))}
-      <Route element={<Navigate to={SHOP_ROUTE} />} />
+      {localStorage.getItem('isAuth') && authRoutes.map(({ path, Component }) =>
+        <Route key={path} path={path} element={<Component />} exact />
+      )}
+      {publicRoutes.map(({ path, Component }) =>
+        <Route key={path} path={path} element={<Component />} exact />
+      )}
+      <Route path='*' element={<Navigate to={MAIN_ROUTE} />} />
     </Routes>
   );
 };
