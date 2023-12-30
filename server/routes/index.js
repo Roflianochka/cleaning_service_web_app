92588955f80e4fa463af554e3d8f03e31fedad47
Roflianochka <maximbelaev831@gmail.com>
2023-12-30 18:01:16 +0300

    feat: add new routers for service assignments and payment transactions

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,8 @@ const servicesRouter = require("./sevicesRouter");
 const servicesReviewsRouter = require("./servicesReviewsRouter");
 const usersRouter = require("./usersRouter");
 const serviceCategoryRouter = require("./serviceCategoryRouter");
+const serviceAssignmentsRouter = require("./serviceAssignmentsRouter")
+const paymentsRouter = require("./paymentTransactionsRouter")
 
 router.use("/user", usersRouter);
 router.use("/employee", employeesRouter);
@@ -13,5 +15,7 @@ router.use("/appointment", appointmentRouter);
 router.use("/service", servicesRouter);
 router.use("/serviceReview", servicesReviewsRouter);
 router.use("/serviceCategory", serviceCategoryRouter);
+router.use("/serviceAssign", serviceAssignmentsRouter);
+router.use("/payment", paymentsRouter);
 
 module.exports = router;
